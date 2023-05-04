4ba9788ab8bcefdc42d4683f786b8ea22ff53feb
ryanC06 <350103776@gapps.yrdsb.ca>
2023-05-04 13:23:52 -0400

    Update MyWorld.java

--- MyWorld.java
+++ MyWorld.java
@@ -8,15 +8,24 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World {
     
-    private int count = 0; // variable to store the count
+    private int count = 0; 
+    //System.Logger.Level countLabel;
+    int level = 1;
+    int eleSpeed = 2;
     
     public MyWorld() {
         super(600, 400, 1);
+        //make hippo
         Hippo hippo = new Hippo();
+        //add count lable 
+        //countLabel Level = new Level (0,80);
+        // add hippo
         addObject(hippo, 50, 350);
+        //make and add apple 
         Apple apple = new Apple();
         addObject(apple, 100, 100);
     }
     
 }
+
 
