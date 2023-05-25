8f61c6f76c621674b01d53fb7b18c38440aab627
ryanC06 <350103776@gapps.yrdsb.ca>
2023-05-25 08:50:15 -0400

    ADDED ALL THE TITLES TO THE TILE PAGE

--- TitleWorld.java
+++ TitleWorld.java
@@ -5,22 +5,36 @@ public class TitleWorld extends World
 {
 
     Label titleLabel = new Label ("Hungry Hippo",60);
-    Label label = new Label ("Use A -- and D to Move",60); 
+    Label label = new Label ("Use A -- and D -- to Move",30); 
+    Label labelc = new Label ("Press <space> to Start",30);
     public TitleWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        addObject(titleLabel, getWidth()/2, getHeight()/2);
-        
+        addObject(titleLabel, 295, 60);
+
+        addObject(label, 295, 320);
+        addObject(labelc, 295, 350);
+
         GreenfootImage bg = new GreenfootImage("images/background3.jpg");
         setBackground(bg);
         bg.scale(600,400);
+        prepare();
     }
-    
+
     public void act(){
         if(Greenfoot.isKeyDown("space")){
             MyWorld gameWorld = new MyWorld();
             Greenfoot.setWorld (gameWorld);
         }
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        
     }
 }
